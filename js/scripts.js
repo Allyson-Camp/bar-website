747ac5faba8837a5f1d84bdf09033026528aa28e
Allyson Camp <Allysoncamp92@gmail.com>
2023-06-01 16:13:29 -0700

    Add another else statement for === 21 with birthday wish and a window alert

--- js/scripts.js
+++ js/scripts.js
@@ -12,16 +12,23 @@ window.onload = function () {
         // see new results.
         let drinkMenu = document.getElementById("drinks");
         let under21Message = document.getElementById("under-21");
+        let birthdayWish = document.getElementById("birthday-wish");
         // we gather the age value
         const age = parseInt(document.querySelector("input#age").value);
-        
+
         //set their attribute to hidden
         drinkMenu.setAttribute("class", "hidden");
         under21Message.setAttribute("class", "hidden");
+        birthdayWish.setAttribute("class", "hidden");
 
         // we check if the age is greater than 21.
-        if (age >= 21) {
+        if (age > 21) {
             drinkMenu.removeAttribute("class");
+            
+        } else if (age === 21) {
+            drinkMenu.removeAttribute("class");
+            birthdayWish.removeAttribute("class");
+            window.alert("You're freshly 21! Remember to drink responsibly!")
             // if its under the else is triggered
         } else {
             under21Message.removeAttribute("class");
